cfe76b05d8ba2d2021978e8d8461fd5dc01e9ce7
sanjay s <sanman014@gmail.com>
2022-07-20 14:04:49 +0530

    Day 3 Task

--- src/simple_app/endpoints.py
+++ src/simple_app/endpoints.py
@@ -1,7 +1,24 @@
 from flask import request,render_template
+import pymongo
+
 def api_routes(endpoints):
     @endpoints.route('/register', methods=['GET'])
     def login():
         return render_template("index.html")
+
+    @endpoints.route('/data', methods=["GET", "POST"])
+    def data():
+        uri = "mongodb+srv://admin:admin@cluster0.qbj8j.mongodb.net/?retryWrites=true&w=majority"
+        client = pymongo.MongoClient(uri)
+        database = client['trainingdb']
+        collection = database['users']
+        data = {}
+        if request.method == "POST":
+            data['email'] = request.form['email']
+            data['name'] = request.form['name']
+            data['pass'] = request.form['pass']
+            data['phno'] = request.form['phno']
+            collection.insert_one(data)
+        return render_template("success.html")
 
     return endpoints
